cbe97293d17e8afb899210fb39c9b63e9a5a22dc
azimshaikh <azim@htsqatar.com>
2022-11-07 14:30:53 +0530

    Initial Commit

---
+++ Imgur Demo/Imgur_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Imgur_DemoApp.swift
+//  Imgur Demo
+//
+//  Created by Aadeez Shaikh on 07/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Imgur_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
